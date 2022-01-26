0912d554359a1b45491a9f7b7482526888cfcc9f
Cristóbal Gallardo <gallardo@informatik.uni-freiburg.de>
2022-01-26 22:21:12 +0100

    Add data

--- src/run_workflow.py
+++ src/run_workflow.py
@@ -1,19 +1,44 @@
 #!/usr/bin/python3
 
-from os import walk,path
 
-workflows_folder = "../workflows/"
-DATA="../data/partial_default_inputs_local.yml"
-COMMD = 'planemo run {} {} --download_outputs --profile LOCAL --history_name {}  --output_directory {}'
-def main():
-    for root, dirs, files in walk(workflows_folder):
+from os import walk,path,makedirs
+from multiprocessing import Process
+from subprocess import Popen, PIPE
+
+
+def launch_planemo(RUN,LOG):
+    process = Popen(RUN, stdout=PIPE)
+    output, error = process.communicate()
+    open(LOG,"a").write(output)            
+
+def generate_RUNS(LOG):
+    WORKFLOWS_FOLDER = "../workflows/"
+    DATA = "../data/partial_default_inputs_local.yml"
+    CMMD = 'planemo run {} {} --profile LOCAL --history_name {}'
+    RUNS = []
+    for root, dirs, files in walk(WORKFLOWS_FOLDER):
         if files:
             workflows_paths = [path.join(root,f) for f in files]
+            global workflow
             for workflow in workflows_paths:
                 HISTORY_NAME = workflow.split("/")[-1].strip(".ga")
-                output_dir = "/".join(workflow.split("/")[:-1]).replace("workflows","outputs")
-                print(output_dir)
-                print(workflow)
+                OUTPUT_PATH = workflow.strip(".ga")
+                if not path.isdir(OUTPUT_PATH):
+                    makedirs(OUTPUT_PATH)
+                RUN = (CMMD.format(workflow,DATA,HISTORY_NAME,OUTPUT_PATH)).split(" ")
+                RUN = [x for x in RUN if x != ""]
+                RUNS.append(RUN)
+    return(RUNS)
+
+def main():
+    LOG = "../outputs/outputs.log"
+    RUNS = generate_RUNS(LOG)
+    processes = [Process(target=launch_planemo, args=(RUN, LOG,)) for RUN in RUNS]
+    for p in processes: p.start()
+    for p in processes: p.join()
+    print(yeah)
+                #process = Popen(RUN, stdout=PIPE)
+                #output, error = process.communicate()
 
 
 if __name__ == "__main__":
